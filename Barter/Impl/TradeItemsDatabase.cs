515845930b2263f4e1fe6ca08e7bb1c56b3e5ff8
kalavarda <test@test.com>
2020-09-06 09:10:35 +0300

    3

--- Barter/Impl/TradeItemsDatabase.cs
+++ Barter/Impl/TradeItemsDatabase.cs
@@ -1,12 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using Barter.Model;
+using Newtonsoft.Json;
 
 namespace Barter.Impl
 {
     public class TradeItemsDatabase: ITradeItemsDatabase
     {
+        private ItemsData _data;
+
+/*
         private IReadOnlyCollection<TradeItem> Level_1_Items = new[]
         {
             new TradeItem
@@ -63,8 +69,29 @@ namespace Barter.Impl
                 Level = TradeItemLevel.Level_5
             },
         };
-
+*/
         private TradeItem[] _all;
+
+        public TradeItemsDatabase()
+        {
+            var fileName = new FileInfo(Assembly.GetExecutingAssembly().Location).DirectoryName;
+            fileName = Path.Combine(fileName, "data.json");
+
+            if (File.Exists(fileName))
+                _data = ItemsData.Load(fileName);
+            else
+            {
+                _data = new ItemsData
+                {
+                    Level_1 = _data.Level_1,
+                    Level_2 = _data.Level_2,
+                    Level_3 = _data.Level_3,
+                    Level_4 = _data.Level_4,
+                    Level_5 = _data.Level_5
+                };
+                _data.Save(fileName);
+            }
+        }
 
         public int GetMass(TradeItemLevel level)
         {
@@ -105,28 +132,63 @@ namespace Barter.Impl
         {
             if (level == null)
                 return _all ??= new TradeItem[0]
-                    .Union(Level_1_Items)
-                    .Union(Level_2_Items)
-                    .Union(Level_3_Items)
-                    .Union(Level_4_Items)
-                    .Union(Level_5_Items)
+                    .Union(_data.Level_1)
+                    .Union(_data.Level_2)
+                    .Union(_data.Level_3)
+                    .Union(_data.Level_4)
+                    .Union(_data.Level_5)
                     .ToArray();
 
             switch (level.Value)
             {
                 case TradeItemLevel.Level_1:
-                    return Level_1_Items;
+                    return _data.Level_1;
                 case TradeItemLevel.Level_2:
-                    return Level_2_Items;
+                    return _data.Level_2;
                 case TradeItemLevel.Level_3:
-                    return Level_3_Items;
+                    return _data.Level_3;
                 case TradeItemLevel.Level_4:
-                    return Level_4_Items;
+                    return _data.Level_4;
                 case TradeItemLevel.Level_5:
-                    return Level_5_Items;
+                    return _data.Level_5;
                 default:
                     throw new NotImplementedException();
             }
+        }
+    }
+
+    public class ItemsData
+    {
+        private static readonly JsonSerializerSettings SerializerSettings = new JsonSerializerSettings
+        {
+            NullValueHandling = NullValueHandling.Ignore,
+            Formatting = Formatting.Indented
+        };
+        private static readonly JsonSerializer JsonSerializer = JsonSerializer.Create(SerializerSettings);
+
+        public TradeItem[] Level_1 { get; set; }
+
+        public TradeItem[] Level_2 { get; set; }
+        
+        public TradeItem[] Level_3 { get; set; }
+        
+        public TradeItem[] Level_4 { get; set; }
+        
+        public TradeItem[] Level_5 { get; set; }
+
+        public void Save(string fileName)
+        {
+            using var file = new FileStream(fileName, FileMode.Create, FileAccess.Write, FileShare.None);
+            using var writer = new StreamWriter(file);
+            JsonSerializer.Serialize(writer, this);
+        }
+
+        public static ItemsData Load(string fileName)
+        {
+            using var file = new FileStream(fileName, FileMode.Open, FileAccess.Read, FileShare.Read);
+            using var reader = new StreamReader(file);
+            using var jsonReader = new JsonTextReader(reader);
+            return JsonSerializer.Deserialize<ItemsData>(jsonReader);
         }
     }
 }
